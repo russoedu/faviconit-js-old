28a306bdce121d69e096c843b190feee17ca9291
Eduardo Russo <russoedu@gmail.com>
2020-03-17 15:36:58 +0000

    languages tested

--- src/controllers/generate.js
+++ src/controllers/generate.js
@@ -1,6 +1,11 @@
+const __ = require('i18n').__
+
 class Generate {
   static render (req, res) {
-    res.render('generate', { page: 'generate' })
+    res.render('generate', {
+      page: 'generate',
+      headMeta: __('download.meta')
+    })
   }
 }
 
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,11 @@
+const __ = require('i18n').__
+
 class Home {
   static render (req, res) {
-    res.render('home', { page: '' })
+    res.render('home', {
+      page: '',
+      headMeta: __('home.meta')
+    })
   }
 }
 
